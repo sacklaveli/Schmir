d2d1b6b2b31f5b2a4a27a3e6d97ca2e350551625
sacklaveli <80595487+sacklaveli@users.noreply.github.com>
2021-03-17 19:03:42 -0600

    readability changes

---
+++ Schmir/BettingLogic/SimpleRules/StandardRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schmear.BettingLogic.SimpleRules
+{
+    public static class StandardRules
+    {
+        public static bool IsSwing(int rank)
+        {
+            return rank >= 12;
+        }
+    }
+}
